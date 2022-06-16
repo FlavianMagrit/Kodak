b3902d7818f19a0d318e791f9d3f1f69f6ad5509
Flavian Magrit <pro.flavianmagrit@gmail.com>
2022-06-16 16:43:05 +0200

    refactor: pp

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -1,28 +1,40 @@
 import './Navbar.scss';
-export const Navbar = () => {
+import { categories, navbarElements } from './categories';
 
-    return (
-        <>
-            <ul className={"top-navbar"}>
-                <li><img src="" alt="Kodak"/></li>
-                <li>Shop</li>
-                <li>Collab</li>
-                <li>App</li>
-                <li>Abonnements</li>
-                <li>Guides & conseils</li>
-                <li><i>...</i></li>
-                <li><i>Panier</i></li>
-            </ul>
-            <ul className={"bottom-navbar"}>
-                <li>Catégorie 1</li>
-                <li>Catégorie 2</li>
-                <li>Catégorie 3</li>
-                <li>Catégorie 4</li>
-                <li>Catégorie 5</li>
-                <li>Catégorie 6</li>
-                <li>Catégorie 7</li>
-                <li>Catégorie 8</li>
-            </ul>
-        </>
-    )
-}
+export const Navbar = () => {
+  return (
+    <>
+      {/*<ul className="top-navbar">*/}
+      {/*  <li>*/}
+      {/*    <img src="" alt="Kodak" />*/}
+      {/*  </li>*/}
+      {/*  <li>Shop</li>*/}
+      {/*  <li>Collab</li>*/}
+      {/*  <li>App</li>*/}
+      {/*  <li>Abonnements</li>*/}
+      {/*  <li>Guides & conseils</li>*/}
+      {/*  <li>*/}
+      {/*    <i>...</i>*/}
+      {/*  </li>*/}
+      {/*  <li>*/}
+      {/*    <i>Panier</i>*/}
+      {/*  </li>*/}
+      {/*</ul>*/}
+      {/*<ul className="bottom-navbar">*/}
+      {/*  {categories.map((el) => (*/}
+      {/*    <li>{el.category}</li>*/}
+      {/*  ))}*/}
+      {/*</ul>*/}
+
+      <div className="test">
+        <div className="test2">
+          <a>Shop</a>
+          <a>Collab</a>
+          <a>App</a>
+          <a>Abonnement</a>
+          <a>Guide et conseil</a>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/containers/Navbar/categories.js
@@ -0,0 +1,28 @@
+export const navbarElements = [{}];
+
+export const categories = [
+  {
+    category: 'Catégorie 1',
+  },
+  {
+    category: 'Catégorie 2',
+  },
+  {
+    category: 'Catégorie 3',
+  },
+  {
+    category: 'Catégorie 4',
+  },
+  {
+    category: 'Catégorie 5',
+  },
+  {
+    category: 'Catégorie 6',
+  },
+  {
+    category: 'Catégorie 7',
+  },
+  {
+    category: 'Catégorie 8',
+  },
+];
