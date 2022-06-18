2dbf11c2fa8caaf7aa8307699ffbe5d34007aa88
AlexandreBecue <alex.becue@live.fr>
2022-06-18 17:06:54 +0200

    [BQ] Navbar update : elements position

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -4,16 +4,24 @@ import { navbarElements } from './categories';
 export const Navbar = () => {
   return (
     <>
-      <div className="navbar">
-        <div className="logo">
-          <a>
-            <img src="" alt="Kodak" />
+      <div className='navbar'>
+        <div className='nav-left'>
+          <a href='home' className='nav-logo'>
+            <img src='' alt='Kodak' />
           </a>
         </div>
-        <div className="nav-items">
+        <div className='nav-center'>
           {navbarElements.map((el) => (
-              <a href={el.route}>{el.label}</a>
+            <a href={el.route} className='nav-item'>{el.label}</a>
           ))}
+        </div>
+        <div className='nav-right'>
+          <a href='profile' className='nav-icon'>
+            <img src='' alt='Profil' />
+          </a>
+          <a href='basket' className='nav-icon'>
+            <img src='' alt='Panier' />
+          </a>
         </div>
       </div>
     </>
