849a1a949019a93fcb3325b0eb2e62279a88a80b
Flavian Magrit <flavian.magrit@decathlon.com>
2022-06-24 10:49:45 +0200

    early return

--- src/components/Input/CustomInput.jsx
+++ src/components/Input/CustomInput.jsx
@@ -1,3 +1,3 @@
-export const CustomInput = ({ placeholder, onChange }) => {
-  return <input placeholder={placeholder} onChange={onChange} />;
-};
+export const CustomInput = ({ placeholder, onChange }) => (
+  <input placeholder={placeholder} onChange={onChange} />
+);
