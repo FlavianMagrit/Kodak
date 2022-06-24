de60ebe9a00c65e2375223231340f5d1a487870b
Gabin BLOQUET <gabin.blqt@gmail.com>
2022-06-24 15:04:23 +0200

    style: authentication page

---
+++ src/containers/SignInOrSignUp/index.jsx
@@ -0,0 +1,37 @@
+import {useContext, useState} from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../utils/firebase-config';
+import {UserContext} from "../../App";
+import {CreateAccount} from "../CreateAccount";
+import {Authentication} from "../Authentication";
+import './Login.scss';
+
+export const SignInOrSignUp = () => {
+  const [accountCreation, setIsAccountCreation] = useState(false);
+
+  const { setUser } = useContext(UserContext);
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  return (
+    <section className='sign-in_sign-up'>
+      <div className="left-side">
+        {accountCreation
+          ? <CreateAccount />
+          : <Authentication />
+        }
+      </div>
+
+      <div className="right-side">
+        { accountCreation
+          ? <a className='create-account' onClick={() => setIsAccountCreation(false)}>Se connecter</a>
+          : <a className='create-account' onClick={() => setIsAccountCreation(true)}>Créer un compte</a>
+        }
+
+        <a className='invite-people' onClick={() => setUser({ email: 'anonyme@anonyme.fr', displayName: 'anonyme'})}>Continuer comme invité</a>
+      </div>
+    </section>
+  );
+};
--- src/pages/AuthenticationPage/index.jsx
+++ src/pages/AuthenticationPage/index.jsx
@@ -1,10 +1,10 @@
-import { Login } from '../containers/Login/Login';
+import { SignInOrSignUp } from '../../containers/SignInOrSignUp';
 
 export const AuthenticationPage = () => {
   return (
     <>
       <h1>Connectez-vous pour régler vos achats plus rapidement !</h1>
-      <Login />
+      <SignInOrSignUp />
     </>
   );
 };
