625854c5f3d50590b9742014a27d85bba1ea4605
AlexandreBecue <alex.becue@live.fr>
2022-07-18 16:06:11 +0200

    [BQ] Fix background image bug

--- src/components/Background/index.jsx
+++ src/components/Background/index.jsx
@@ -1,10 +1,8 @@
-import PictureBackground from '../../assets/background.jpeg';
 import './Background.scss';
 
 export const Background = ({ title, pointColor }) => (
   <div className="background-container flex jcc aic">
-    <img src={PictureBackground} alt="Background" className="background-image" />
-    <div className="title-container absolute w75">
+    <div className="title-container">
       <h1 className="white tac">
         {title} <span className={`${pointColor} rounded-point`}>.</span>
       </h1>
