3bc7d120631d41a3074219d1b8dbbe9e44f4c594
AlexandreBecue <alex.becue@live.fr>
2022-07-20 14:46:12 +0200

    [BQ] Responsive section title

--- src/components/SectionTitle/index.jsx
+++ src/components/SectionTitle/index.jsx
@@ -1,5 +1,7 @@
+import './SectionTitle.scss';
+
 export const SectionTitle = ({ title, pointColor, className }) => (
-  <h1 className={`ml-2 ${className}`}>
+  <h1 className={`${className}`}>
     {title}
     <span className={`${pointColor} rounded-point`}>.</span>
   </h1>
