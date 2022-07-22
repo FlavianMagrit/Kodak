8f6159022d28dad15a36bd60326f3b55f75d23cc
AlexandreBecue <alex.becue@live.fr>
2022-07-22 10:53:46 +0200

    [BQ] Responsive loginpage

--- src/pages/AuthenticationPage/index.jsx
+++ src/pages/AuthenticationPage/index.jsx
@@ -1,12 +1,17 @@
 import { SignInOrSignUp } from '../../containers/SignInOrSignUp';
+import { Footer } from '../../containers/Footer';
+import Logo from '../../assets/logo-kodak.png';
+import './AuthenticationPage.scss';
 
 export const AuthenticationPage = () => {
   return (
     <div className="flex-column jcc aic mt-2">
-      <h2 className="mt-2 mb-2">
-        Connectez-vous pour régler vos achats plus rapidement !
-      </h2>
-      <SignInOrSignUp />
+      <img src={Logo} alt="Kodak" />
+      <div className="authent-container tac">
+        <h2 className="mb-2">Connectez-vous pour régler vos achats plus rapidement !</h2>
+        <SignInOrSignUp />
+      </div>
+      <Footer />
     </div>
   );
 };
