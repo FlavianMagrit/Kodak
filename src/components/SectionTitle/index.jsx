cc3e73010e01349b6d18bad008ec8e5511589dd0
Flavian Magrit <flavian.magrit@decathlon.com>
2022-07-24 21:29:27 +0200

    returns

--- src/components/SectionTitle/index.jsx
+++ src/components/SectionTitle/index.jsx
@@ -1,7 +1,7 @@
 import './SectionTitle.scss';
 
 export const SectionTitle = ({ title, pointColor, className }) => (
-  <h1 className={`${className}`}>
+  <h1 className={className}>
     {title}
     <span className={`${pointColor} rounded-point`}>.</span>
   </h1>
